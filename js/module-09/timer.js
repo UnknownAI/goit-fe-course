8da0c4634e611e3350720c02e2615408cd9d6f21
Agniya <ivanishyna@gmail.com>
2018-07-26 13:45:57 +0300

    new hometask

--- js/module-09/timer.js
+++ js/module-09/timer.js
@@ -1,30 +1,38 @@
-let startTime = Date.now(); 
-let clockFace = document.querySelector('.js-time');
-let startBtn = document.querySelector('.js-start'); 
+'use strict'
+
+const clockFace = document.querySelector('.js-time');
+const startBtn = document.querySelector('.js-start'); 
 const resetBtn = document.querySelector('.js-reset');
 const lapBtn = document.querySelector('.js-take-lap'); 
 const container = document.querySelector('.js-laps'); 
+let startTime = Date.now(); 
 let timerId = 0; 
 let pausedTime = 0;
-let isActive = false; 
-let currentTime = 0; 
+let currentTime = 0;
+let isActive = false;  
 
 
 function timer(time){ 
     currentTime =  new Date(time);
     let minutes = currentTime.getMinutes() < 10 ? `0${currentTime.getMinutes()}` : currentTime.getMinutes(); 
     let seconds = currentTime.getSeconds() < 10 ? `0${currentTime.getSeconds()}` : currentTime.getSeconds();
-    let mSec = Number.parseInt(currentTime.getMilliseconds()/100);
-    return `${minutes}:${seconds}.${mSec}`;
+    let milliseconds = Number.parseInt(currentTime.getMilliseconds()/100);
+    return `${minutes}:${seconds}.${milliseconds}`;
 }
 
 
-function timerFace(){
+function showTime(){
     clockFace.textContent = timer(Date.now() - startTime); 
 }
 
-function onStart() {
+function activateTimer(time){
+    startBtn.textContent = 'Pause';
+    startTime = time; 
+    timerId = setInterval(showTime,100); 
+    isActive = true; 
+}
 
+function onStart() {
     if (!isActive) {
         if (currentTime === 0) {
             activateTimer(Date.now());
@@ -39,34 +47,25 @@ function onStart() {
     }
 }
 
-function activateTimer(time){
-    startBtn.textContent = 'Pause';
-    startTime = time; 
-    timerId = setInterval(timerFace,100); 
-    isActive = true; 
-}
-
-function onReset(){
-    clearInterval(timerId); 
-    clockFace.textContent = timer(0); 
-    startBtn.textContent = 'Start'; 
-    currentTime = 0; 
-    
-    while (container.firstChild){
-        container.removeChild(container.firstChild); 
-    }
-}
-
 function onLap(){
     const listElement = document.createElement('li'); 
     listElement.textContent = clockFace.textContent;
-    container.appendChild(listElement); 
-    
+    container.appendChild(listElement);    
 }
 
+function onReset() {
+    clearInterval(timerId);
+    isActive = false;
+    clockFace.textContent = timer(0);
+    pausedTime = 0;
+    startBtn.textContent = 'Start';
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}   
 
-resetBtn.addEventListener('click',onReset); 
+
 startBtn.addEventListener('click',onStart); 
 lapBtn.addEventListener('click', onLap); 
-
+resetBtn.addEventListener('click',onReset); 
 
